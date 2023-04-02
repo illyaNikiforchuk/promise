28c25227e490afab528df0132f8ee52755e6eb28
illyaNikiforchuk <116200971+illyaNikiforchuk@users.noreply.github.com>
2023-04-02 17:06:36 +0300

    visa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ReactComponent as PassportIcon } from "./background_img/done.svg";
+// import { ReactComponent as RejectIcon } from "./background_img/reject.svg";
+import { ReactComponent as ResolveIcon } from "./background_img/resolve.svg";
 
-function App() {
+const Visa = () => {
+  const doc = [
+    { status: "start" },
+    { status: "checking your passport", icon: null },
+    {
+      status: "passport is checked",
+      icon: <PassportIcon className="svgIcon" />,
+    },
+    { status: "checking your scans and copies", icon: null },
+    { status: "your scan ok", icon: <PassportIcon className="svgIcon" /> },
+    { status: "passport checked", icon: <PassportIcon className="svgIcon" /> },
+    { status: "all scans is ok", icon: <PassportIcon className="svgIcon" /> },
+    {
+      status: "ua passport also ok",
+      icon: <PassportIcon className="svgIcon" />,
+    },
+    {
+      status: "marriage - you meried",
+      icon: <PassportIcon className="svgIcon" />,
+    },
+    {
+      status: "you don't have any children",
+      icon: <PassportIcon className="svgIcon" />,
+    },
+    { status: "you're  poor", icon: <PassportIcon className="svgIcon" /> },
+  ];
+
+  const [output, setOutput] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const validation = () => {
+    setOutput([]);
+    setCount(0);
+
+    const validateStep = (index) => {
+      setTimeout(() => {
+        if (index >= doc.length) {
+          setOutput((prevOutput) => [
+            ...prevOutput,
+            {
+              count: prevOutput.length + 1,
+              status: "finish",
+              icon: <ResolveIcon className="svgIcon" />,
+            },
+          ]);
+          return;
+        }
+
+        const currentStep = doc[index];
+
+        setOutput((prevOutput) => [
+          ...prevOutput,
+          {
+            count: prevOutput.length + 1,
+            status: currentStep.status,
+            icon: currentStep.icon,
+          },
+        ]);
+
+        validateStep(index + 1);
+      }, 2000);
+    };
+
+    validateStep(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button className="validation" onClick={validation}>
+        To Audit
+      </button>
+
+      <ul className="ulHTML">
+        {output.map((item) => (
+          <li className="liItem" key={item.count}>
+            <p className="pItem">{item.status}</p>
+            {item.icon}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default Visa;
